467aeaed8cf544c74f22da270cb9af3220c5a415
briantberger <brianberger23@gmail.com>
2024-01-02 20:51:21 -0800

    added tests

--- app/src/test/java/com/example/budgetplanner/PurchasesTests.kt
+++ app/src/test/java/com/example/budgetplanner/PurchasesTests.kt
@@ -29,4 +29,67 @@ class PurchasesTests {
         assertEquals(BigDecimal("48.50"), groceryPurchases.getGrandTotal())
     }
 
+    @Test
+    fun addTwoPurchases() {
+        val saveOnPurchase = Purchase("Save-On-Foods", BigDecimal("48.50"))
+        val urbanFarePurchase = Purchase("Urban Fare", BigDecimal("21.50"))
+        groceryPurchases.addPurchase(saveOnPurchase)
+        groceryPurchases.addPurchase(urbanFarePurchase)
+
+        assertEquals(2, groceryPurchases.purchases.size)
+        assertTrue(groceryPurchases.purchases.contains(saveOnPurchase))
+        assertTrue(groceryPurchases.purchases.contains(urbanFarePurchase))
+        assertEquals(BigDecimal("70.00"), groceryPurchases.getGrandTotal())
+    }
+
+    @Test
+    fun addTwoIdenticalPurchases() {
+        val amazonPrimePurchase1 = Purchase("Amazon", BigDecimal("9.99"))
+        val amazonPrimePurchase2 = Purchase("Amazon", BigDecimal("9.99"))
+        assertEquals(amazonPrimePurchase2, amazonPrimePurchase1)
+
+        groceryPurchases.addPurchase(amazonPrimePurchase1)
+        groceryPurchases.addPurchase(amazonPrimePurchase2)
+
+        assertEquals(2, groceryPurchases.purchases.size)
+        assertTrue(groceryPurchases.purchases.contains(amazonPrimePurchase2))
+        assertTrue(groceryPurchases.purchases.contains(amazonPrimePurchase1))
+        assertEquals(BigDecimal("19.98"), groceryPurchases.getGrandTotal())
+    }
+
+    @Test
+    fun deleteOnePurchase() {
+        val saveOnPurchase: Purchase = Purchase("Save-On-Foods", BigDecimal("48.50"))
+        groceryPurchases.addPurchase(saveOnPurchase)
+        assertEquals(1, groceryPurchases.purchases.size)
+        assertEquals(BigDecimal("48.50"), groceryPurchases.getGrandTotal())
+
+        groceryPurchases.deletePurchase(saveOnPurchase)
+        assertEquals(0, groceryPurchases.purchases.size)
+        assertEquals(BigDecimal("0"), groceryPurchases.getGrandTotal())
+    }
+
+    @Test
+    fun deleteOneIdenticalPurchase() {
+        val amazonPrimePurchase1 = Purchase("Amazon", BigDecimal("9.99"))
+        val amazonPrimePurchase2 = Purchase("Amazon", BigDecimal("9.99"))
+        assertEquals(amazonPrimePurchase2, amazonPrimePurchase1)
+
+        groceryPurchases.addPurchase(amazonPrimePurchase1)
+        groceryPurchases.addPurchase(amazonPrimePurchase2)
+
+        // standard testing before removal, for sanity
+        assertEquals(2, groceryPurchases.purchases.size)
+        assertTrue(groceryPurchases.purchases.contains(amazonPrimePurchase2))
+        assertTrue(groceryPurchases.purchases.contains(amazonPrimePurchase1))
+        assertEquals(BigDecimal("19.98"), groceryPurchases.getGrandTotal())
+
+        groceryPurchases.deletePurchase(amazonPrimePurchase2)
+        assertEquals(1, groceryPurchases.purchases.size)
+        assertEquals(BigDecimal("9.99"), groceryPurchases.getGrandTotal())
+        // check both because they should be equivalent, but only one should be removed.
+        assertTrue(groceryPurchases.purchases.contains(amazonPrimePurchase2))
+        assertTrue(groceryPurchases.purchases.contains(amazonPrimePurchase1))
+    }
+
 }
